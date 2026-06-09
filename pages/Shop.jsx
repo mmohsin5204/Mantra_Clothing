5ecdd234c4010d7a12af2d043066b3a23842da29
mmohsin5204 <m82186445@gmail.com>
2026-06-09 15:30:31 -0700

    Updated Sizzle BBQ project

--- pages/Shop.jsx
+++ pages/Shop.jsx
@@ -57,7 +57,7 @@ export const Shop = () => {
         {/* Right: Product Grid */}
         <div className="w-[70%] px-4 pt-4">
           {filteredProducts.length > 0 ? (
-            <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4">
               {filteredProducts.map(product => (
                 <ProductCard key={product.id} product={product} />
               ))}
